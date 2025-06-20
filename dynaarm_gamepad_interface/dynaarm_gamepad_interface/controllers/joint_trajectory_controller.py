6ffc85e737403c9dd63e97c2ffa06ab169018e0b
svenkae1234 <sven.kaehler@freenet.de>
2025-06-20 20:35:55 +0200

    Added a driving in home position function

--- dynaarm_gamepad_interface/dynaarm_gamepad_interface/controllers/joint_trajectory_controller.py
+++ dynaarm_gamepad_interface/dynaarm_gamepad_interface/controllers/joint_trajectory_controller.py
@@ -23,6 +23,9 @@
 
 from dynaarm_gamepad_interface.controllers.base_controller import BaseController
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from sensor_msgs.msg import JointState
+from rclpy.duration import Duration
+import rclpy
 
 
 class JointTrajectoryController(BaseController):
@@ -34,6 +37,12 @@ class JointTrajectoryController(BaseController):
         super().__init__(node)
         topic_prefix = "/joint_trajectory_controller"
         found_topics = self.get_topic_names_and_types(f"{topic_prefix}*/joint_trajectory")
+
+        self.home_position = [0.0] * 6
+        self.step_size_flexion_joints = 0.002  # joints 2,3,5
+        self.step_size_rotation_joints= 0.005  # other joints
+        self.flexion_joints_indicies = [1, 2, 4]  # Joints 2, 3, 5
+        self.rotation_joints_indicies = [i for i in range(6) if i not in self.flexion_joints_indicies]
 
         self.mirror = self.node.get_parameter("mirror").get_parameter_value().bool_value
         self.joint_trajectory_publishers = {}
@@ -80,6 +89,7 @@ class JointTrajectoryController(BaseController):
         if self.mirrored_joints:
             self.node.get_logger().info(f"Mirrored joints: {self.mirrored_joints}")
 
+
     def reset(self):
         """Reset commanded positions to current joint states for all topics."""
         joint_states_list = self.get_joint_states()  # Returns a list of dicts
@@ -101,55 +111,58 @@ class JointTrajectoryController(BaseController):
     def process_input(self, msg):
         """Processes joystick input, integrates over dt, and clamps the commanded positions."""
         super().process_input(msg)  # For any base logging logic
-
         any_axis_active = False
         deadzone = 0.1
 
         # Process each topic (arm/controller) independently
         for topic, joint_names in self.topic_to_joint_names.items():
             commanded_positions = self.topic_to_commanded_positions[topic]
-            for i, joint_name in enumerate(joint_names):
-                axis_val = 0.0
+            if msg.buttons[1]:
+                commanded_positions = self.move_to_home(joint_names)
+                any_axis_active = True
+            else:
+                for i, joint_name in enumerate(joint_names):
+                    axis_val = 0.0
 
-                # Map axes/buttons as needed for each joint index
-                if i == 0 and len(msg.axes) > self.node.axis_mapping["left_joystick"]["x"]:
-                    axis_val = msg.axes[self.node.axis_mapping["left_joystick"]["x"]]
-                elif i == 1 and len(msg.axes) > self.node.axis_mapping["left_joystick"]["y"]:
-                    axis_val = msg.axes[self.node.axis_mapping["left_joystick"]["y"]]
-                elif i == 2 and len(msg.axes) > self.node.axis_mapping["right_joystick"]["y"]:
-                    axis_val = msg.axes[self.node.axis_mapping["right_joystick"]["y"]]
-                elif i == 3 and len(msg.axes) > self.node.axis_mapping["right_joystick"]["x"]:
-                    axis_val = msg.axes[self.node.axis_mapping["right_joystick"]["x"]]
-                elif i == 4:
-                    left_trigger = msg.axes[self.node.axis_mapping["triggers"]["left"]]
-                    right_trigger = msg.axes[self.node.axis_mapping["triggers"]["right"]]
-                    axis_val = right_trigger - left_trigger
-                elif i == 5:
-                    move_left = msg.buttons[self.node.button_mapping["wrist_rotation_left"]] == 1
-                    move_right = msg.buttons[self.node.button_mapping["wrist_rotation_right"]] == 1
-                    if move_left and not move_right:
-                        axis_val = -1.0
-                    elif move_right and not move_left:
-                        axis_val = 1.0
+                    # Map axes/buttons as needed for each joint index
+                    if i == 0 and len(msg.axes) > self.node.axis_mapping["left_joystick"]["x"]:
+                        axis_val = msg.axes[self.node.axis_mapping["left_joystick"]["x"]]
+                    elif i == 1 and len(msg.axes) > self.node.axis_mapping["left_joystick"]["y"]:
+                        axis_val = msg.axes[self.node.axis_mapping["left_joystick"]["y"]]
+                    elif i == 2 and len(msg.axes) > self.node.axis_mapping["right_joystick"]["y"]:
+                        axis_val = msg.axes[self.node.axis_mapping["right_joystick"]["y"]]
+                    elif i == 3 and len(msg.axes) > self.node.axis_mapping["right_joystick"]["x"]:
+                        axis_val = msg.axes[self.node.axis_mapping["right_joystick"]["x"]]
+                    elif i == 4:
+                        left_trigger = msg.axes[self.node.axis_mapping["triggers"]["left"]]
+                        right_trigger = msg.axes[self.node.axis_mapping["triggers"]["right"]]
+                        axis_val = right_trigger - left_trigger
+                    elif i == 5:
+                        move_left = msg.buttons[self.node.button_mapping["wrist_rotation_left"]] == 1
+                        move_right = msg.buttons[self.node.button_mapping["wrist_rotation_right"]] == 1
+                        if move_left and not move_right:
+                            axis_val = -1.0
+                        elif move_right and not move_left:
+                            axis_val = 1.0
 
-                if self.mirror and joint_name in self.mirrored_joints:
-                    axis_val = -axis_val
+                    if self.mirror and joint_name in self.mirrored_joints:
+                        axis_val = -axis_val
 
-                if abs(axis_val) > deadzone:
-                    current_position = self.node.joint_states.get(joint_name, 0.0)
-                    commanded_positions[i] += axis_val * self.node.dt
-                    offset = commanded_positions[i] - current_position
-                    if offset > self.node.joint_pos_offset_tolerance:
-                        commanded_positions[i] = (
-                            current_position + self.node.joint_pos_offset_tolerance
-                        )
-                        self.node.gamepad_feedback.send_feedback(intensity=1.0)
-                    elif offset < -self.node.joint_pos_offset_tolerance:
-                        commanded_positions[i] = (
-                            current_position - self.node.joint_pos_offset_tolerance
-                        )
-                        self.node.gamepad_feedback.send_feedback(intensity=1.0)
-                    any_axis_active = True
+                    if abs(axis_val) > deadzone:
+                        current_position = self.node.joint_states.get(joint_name, 0.0)
+                        commanded_positions[i] += axis_val * self.node.dt
+                        offset = commanded_positions[i] - current_position
+                        if offset > self.node.joint_pos_offset_tolerance:
+                            commanded_positions[i] = (
+                                current_position + self.node.joint_pos_offset_tolerance
+                            )
+                            self.node.gamepad_feedback.send_feedback(intensity=1.0)
+                        elif offset < -self.node.joint_pos_offset_tolerance:
+                            commanded_positions[i] = (
+                                current_position - self.node.joint_pos_offset_tolerance
+                            )
+                            self.node.gamepad_feedback.send_feedback(intensity=1.0)
+                        any_axis_active = True
 
             # Update the commanded positions for this topic
             self.topic_to_commanded_positions[topic] = commanded_positions
@@ -172,6 +185,62 @@ class JointTrajectoryController(BaseController):
                     joint_names=self.topic_to_joint_names[topic],
                 )
             self.is_joystick_idle = True
+
+    def joints_at_home(self, current, indices, tolerance=0.01):
+        return all(abs(current[i] - self.home_position[i]) < tolerance for i in indices)
+    
+    # Returns the movement phase based on current joint positions
+    # 0 = all joints at home, 1 = flexion joints needs moving, 2 = rotation joints needs moving
+    def get_movement_phase(self, current_joints):
+
+        flexion_done = self.joints_at_home(current_joints, self.flexion_joints_indicies)
+        rotation_done = self.joints_at_home(current_joints, self.rotation_joints_indicies)
+
+        if flexion_done:
+            if rotation_done:
+                return 0  # All joints at home
+            else:
+                return 2  # Only rotation joints needs moving
+        else:
+            return 1  # flexion joints needs moving
+        
+    def extract_joint_values(self, all_joint_states, joint_names):
+        for joint_dict in all_joint_states:
+            if all(name in joint_dict for name in joint_names):
+                return [joint_dict[name] for name in joint_names]
+        return []  # Not found
+
+    def move_to_home(self, joint_names):
+        current_joint_values = self.extract_joint_values(self.get_joint_states(), joint_names)
+        self.movement_phase = self.get_movement_phase(current_joint_values) # Describes which joints need moving, flexion, rotation or all at home
+
+        try:
+            if self.movement_phase == 1:
+                next_step = self.interpolate_partial(
+                    current_joint_values, self.home_position, self.flexion_joints_indicies, self.step_size_flexion_joints)
+                return next_step
+
+            elif self.movement_phase == 2:
+                next_step = self.interpolate_partial(
+                    current_joint_values, self.home_position, self.rotation_joints_indicies, self.step_size_rotation_joints)
+                return next_step
+
+            elif self.movement_phase == 0:
+                self.node.get_logger().info("All joints already at home position.")
+                return current_joint_values
+
+        except KeyboardInterrupt:
+            print("Keyboard interrupt received, stopping home movement.")
+
+    def interpolate_partial(self, current, target, indices, step_size):
+        next_step = current[:]
+        for i in indices:
+            delta = target[i] - current[i]
+            if abs(delta) > step_size:
+                next_step[i] = current[i] + step_size * (1 if delta > 0 else -1)
+            else:
+                next_step[i] = target[i]
+        return next_step
 
     def get_joint_base_name(self, joint_name):
         # Assumes joint names are like 'shoulder_rotation_arm_left'
